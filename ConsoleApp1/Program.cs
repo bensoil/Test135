aae9ab8f9035ef8570a1861d33b2ad3d85edf9e4
bensoil <157688179+bensoil@users.noreply.github.com>
2024-05-08 14:36:15 +0200

    Gjort oppgaven

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("What is up, dawg");
+Console.WriteLine("Hei, hva heter du?");
+var userInput = Console.ReadLine();
+Console.WriteLine($"Velkommen {userInput}");
